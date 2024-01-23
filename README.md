9c781aca4d67afda6c76ca34d4f84e40750544d4
Nate Bosch <nbosch@google.com>
2024-01-23 06:02:00 +0000

    Implement a Dart Gemini client and samples
    Design follows the general approach of the generative AI SDKs in other
    languages.
    
    A `GenerativeModel` class is initialized with an API key and a
    chosen model. The model communicates with the remote API using REST
    requests. It implements the `generateContent`, `generateContentStream`,
    `countTokens`, `embedContent` APIs. Outer level "request" objects are
    not exposed - the fields of those objects are the arguments to the model
    methods. This reduces noise at the callsites by avoiding a nominal
    constructor with a long name.
    
    Implement a `text` getter on the response to generate content requests
    which simplifies extracting a String from the candidate and part lists
    in the common case of wanting the starting text from the first
    candidate.
    
    A `ChatSession` can be created from a model by calling an extension
    method. The chat session holds chat history
    
    Data models, translation to json and parsing from json, are hand-written
    against the proto definitions for the API. We may consider
    autogenerating these from some source in the future.
    
    The `Content` data model is the primary argument for most of the service
    methods, and has a very general design which specifies a "role" and
    allows the content to come in multiple parts. The typical case for
    arguments to the generative model methods will be to use the "user" role
    and a single part. Add a `Content.text`, `Content.data`, and
    `Content.multi` helpers for creating content with a user role.
    
    Other repo changes:
    - Add Dart related local files to `.gitignore.`
    - Update README with dart code samples and early usage instructions.

--- README.md
+++ README.md
@@ -1,31 +1,55 @@
 # Google Generative AI SDK for Dart
 
-The Google Generative AI SDK for Dart allows developers to use state-of-the-art Large Language Models (LLMs) to build language applications.
-
+The Google Generative AI SDK for Dart allows developers to use state-of-the-art
+Large Language Models (LLMs) to build language applications.
 
 ## Getting Started
 
-TODO: This repository contains a few sample apps. To try them out, follow these steps:
+This repository contains a few sample apps. To try them out, follow these
+steps:
 
+- Change directory to the samples package.
+- Export a `$GOOGLE_API_KEY` environment variable with an API key with access to
+  the Gemini generative models, or run the below commands with an environment
+  containing this variable.
+- Run any sample from the `bin` directory. `dart bin/simple_text.dart`.
 
 ## Using the SDK in your own app
 
+Add a dependency on this repository to the path `pkgs/google_generative_ai`.
+
+```yaml
+dependencies:
+  google_generative_ai:
+    git:
+      url: git@github.com:google/generative-ai-dart.git
+      path: pkgs/google_generative_ai
+      ref: main
+```
 
 ### Initializing the API client
 
+```dart
+  final model = GenerativeModel(model: 'gemini-pro', apiKey: apiKey);
+```
 
 ### Calling the API
 
-
+```dart
+  final prompt = 'Write a story about a magic backpack.';
+  final content = [Content.text(prompt)];
+  final response = await model.generateContent(content);
+  print(response.text);
+```
 
 ## Documentation
 
-TODO: You can find the documentation for the 
+TODO: You can find the documentation for the
 
 ## Contributing
 
-See [Contributing](docs/CONTRIBUTING.md) for more information on contributing to the Generative AI SDK for Dart.
-
+See [Contributing](docs/CONTRIBUTING.md) for more information on contributing to
+the Generative AI SDK for Dart.
 
 ## License
 
---
+++ samples/bin/advanced_text_and_image.dart
@@ -0,0 +1,60 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:google_generative_ai/google_generative_ai.dart';
+
+import 'util/resource.dart';
+
+void main() async {
+  final apiKey = Platform.environment['GOOGLE_API_KEY'];
+  if (apiKey == null) {
+    print('No \$GOOGLE_API_KEY environment variable');
+    exit(1);
+  }
+  final model = GenerativeModel(
+      model: 'gemini-pro-vision',
+      apiKey: apiKey,
+      generationConfig: GenerationConfig(temperature: 0));
+  final prompt =
+      'What do you see? Use lists. Start with a headline for each image.';
+  print('Prompt: $prompt');
+
+  late final Uint8List catBytes, sconeBytes;
+  try {
+    (catBytes, sconeBytes) = await (
+      readResource('cat.jpg'),
+      readResource('scones.jpg'),
+    ).wait;
+  } on ParallelWaitError catch (e) {
+    print('Error:');
+    e.errors.forEach(print);
+  }
+  final content = [
+    Content.multi([Text(prompt),
+    // The only accepted mime types are image/*.
+    Data('image/jpeg', catBytes),
+    Data('image/jpeg', sconeBytes),])
+  ];
+  // final tokenCount = await model.countTokens(content);
+  // print('Token count: ${tokenCount.totalTokens}');
+
+  final responses = model.generateContentStream(content);
+  await for (final response in responses) {
+    stdout.write(response.text);
+  }
+  stdout.writeln();
+}
