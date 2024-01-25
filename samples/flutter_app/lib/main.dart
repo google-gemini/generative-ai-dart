b48befe8dcdb809f87670da4ebf7233f8009db04
John Ryan <ryjohn@google.com>
2024-01-25 11:18:01 -0800

    use flutter_markdown package

--- samples/flutter_app/lib/main.dart
+++ samples/flutter_app/lib/main.dart
@@ -14,6 +14,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:google_generative_ai/google_generative_ai.dart' as gen_ai;
+import 'package:flutter_markdown/flutter_markdown.dart';
 
 void main() {
   runApp(const GenerativeAISample());
@@ -208,9 +209,7 @@ class MessageWidget extends StatelessWidget {
             ),
             padding: const EdgeInsets.all(8.0),
             margin: const EdgeInsets.only(bottom: 8),
-            child: SelectionArea(
-              child: Text(text),
-            ),
+            child: MarkdownBody(data: text),
           ),
         ),
       ],
