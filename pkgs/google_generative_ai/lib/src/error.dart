0a181e5a3aa13c13becd0802156a479e01b044ca
Nate Bosch <nbosch@google.com>
2024-02-08 02:04:34 +0000

    Add parsing for unary server errors
    When parsing to a `GenerateContentResponse` fails with a format
    exception, it may be due to the server responding with an "error"
    response.
    
    Add specific exception types for `InvalidApiKey` and
    `UnsupportedUserLocation` since these errors may need specific handling
    in a client. For now all other errors are a `ServerException`, but we
    can add other types if it becomes useful to match more specific
    patterns.
    
    Add tests with scraped server error responses parsed to each error type.
    
    Export the errors from the package public API.
    
    Add a doc comment to the `GenerativeAIException` class from
    https://github.com/google/generative-ai-dart/pull/28/files#diff-422b60b8e6460903ebe4ea03bef1a87c07ba50229d3f46bf436dc8ddd905d050
    
    Fix a typo in the exception `toString()`.

--- pkgs/google_generative_ai/lib/src/error.dart
+++ pkgs/google_generative_ai/lib/src/error.dart
@@ -12,11 +12,56 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+/// Exception thrown when generating content fails.
+///
+/// The [message] may explain the cause of the failure.
 final class GenerativeAIException implements Exception {
   final String message;
 
   GenerativeAIException(this.message);
 
   @override
-  String toString() => 'GenerateiveAIException: $message';
+  String toString() => 'GenerativeAIException: $message';
+}
+
+/// Exception thrown when the server rejects the API key.
+final class InvalidApiKey implements GenerativeAIException {
+  @override
+  final String message;
+
+  InvalidApiKey(this.message);
+
+  @override
+  String toString() => message;
+}
+
+/// Exception thrown when the user location is unsupported.
+final class UnsupportedUserLocation implements GenerativeAIException {
+  static const _message = 'User location is not supported for the API use.';
+  @override
+  String get message => _message;
+}
+
+/// Exception thrown when the server failed to generate content.
+final class ServerException implements GenerativeAIException {
+  @override
+  final String message;
+
+  ServerException(this.message);
+
+  @override
+  String toString() => message;
+}
+
+GenerativeAIException parseError(Object jsonObject) {
+  return switch (jsonObject) {
+    {
+      'message': final String message,
+      'details': [{'reason': 'API_KEY_INVALID'}, ...]
+    } =>
+      InvalidApiKey(message),
+    {'message': UnsupportedUserLocation._message} => UnsupportedUserLocation(),
+    {'message': final String message} => ServerException(message),
+    _ => throw FormatException('Unhandled Server Error format', jsonObject)
+  };
 }
