4ce77da0a1aefafa0cdc6e026c7bd9fb458bb6c7
Brian Quinlan <bquinlan@google.com>
2024-02-13 10:56:54 -0800

    Fix a bug where the developer's `Client` was not used in unary requests (#65)

--- pkgs/google_generative_ai/test/http_api_client_test.dart
+++ pkgs/google_generative_ai/test/http_api_client_test.dart
@@ -23,7 +23,7 @@ import 'utils/matchers.dart';
 
 void main() {
   group('HttpApiClient', () {
-    test('can make unary request', () async {
+    test('can make unary request with default client', () async {
       final url = Uri.parse('https://someurl.com');
       final body = {'some': 'body'};
       final apiKey = 'apiKey';
@@ -48,7 +48,30 @@ void main() {
               }));
     });
 
-    test('can make streaming request', () async {
+    test('can make unary request with custom client', () async {
+      final url = Uri.parse('https://someurl.com');
+      final body = {'some': 'body'};
+      final apiKey = 'apiKey';
+      final expectedResponse = {'result': 'OK'};
+      final httpClient = MockClient((request) async {
+        expect(
+            request,
+            matchesRequest(http.Request('POST', url)
+              ..headers.addAll({
+                'x-goog-api-key': apiKey,
+                'x-goog-api-client': clientName,
+                'Content-Type': 'application/json'
+              })
+              ..bodyBytes = utf8.encode(jsonEncode(body))));
+        return http.Response.bytes(
+            utf8.encode(jsonEncode(expectedResponse)), 200);
+      });
+      final client = HttpApiClient(apiKey: apiKey, httpClient: httpClient);
+      final response = await client.makeRequest(url, body);
+      expect(response, expectedResponse);
+    });
+
+    test('can make streaming request with default client', () async {
       final url = Uri.parse('https://someurl.com');
       final streamingUrl = Uri.parse('https://someurl.com?alt=sse');
       final body = {'some': 'body'};
@@ -79,6 +102,37 @@ void main() {
                         .map((r) => utf8.encode('data: ${jsonEncode(r)}\n')),
                     200);
               }));
+    });
+
+    test('can make streaming request with custom client', () async {
+      final url = Uri.parse('https://someurl.com');
+      final streamingUrl = Uri.parse('https://someurl.com?alt=sse');
+      final body = {'some': 'body'};
+      final apiKey = 'apiKey';
+      final expectedResponses = [
+        {'first': 'OK'},
+        {'second': 'OK'}
+      ];
+      final httpClient = MockClient.streaming((request, requestStream) async {
+        expect(
+            request,
+            matchesBaseRequest(http.Request('POST', streamingUrl)
+              ..headers.addAll({
+                'x-goog-api-key': apiKey,
+                'x-goog-api-client': clientName,
+                'Content-Type': 'application/json'
+              })));
+        expect(requestStream,
+            emitsInOrder([utf8.encode(jsonEncode(body)), emitsDone]));
+        return http.StreamedResponse(
+            Stream.fromIterable(expectedResponses)
+                .map((r) => utf8.encode('data: ${jsonEncode(r)}\n')),
+            200);
+      });
+      final client = HttpApiClient(apiKey: apiKey, httpClient: httpClient);
+      final response = client.streamRequest(url, body);
+      await expectLater(
+          response, emitsInOrder([...expectedResponses, emitsDone]));
     });
   });
 }
