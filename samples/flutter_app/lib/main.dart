348a7593ea20f20b7139fc2bd82233a31033361b
Cynthia J <cynthiajoan@gmail.com>
2024-02-15 14:03:27 -0800

    fix the analyze error

--- samples/flutter_app/lib/main.dart
+++ samples/flutter_app/lib/main.dart
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
