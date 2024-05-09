2112625b60d422c60f01874d39bf86c6a6ba13b1
Nate Bosch <nbosch@google.com>
2024-05-09 22:20:00 +0000

    Unused import

--- pkgs/google_generative_ai/test/utils/stub_client.dart
+++ pkgs/google_generative_ai/test/utils/stub_client.dart
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import 'dart:async';
 import 'dart:collection';
 
 import 'package:collection/collection.dart';
